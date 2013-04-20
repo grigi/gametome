0c71d4ad5e6d67115998ca3ba1e1ff944dc5101e
Grigi <nagrigoriadis@gmail.com>
2013-04-20 20:52:47 +0200

    Pagination

--- gtdb/views.py
+++ gtdb/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
 from .models import *
+
+RESULTS_PER_PAGE = getattr(settings, 'RESULTS_PER_PAGE', 20)
 
 # Create your views here.
 
 def index(request):
     ct = ContentType.objects.get(model='news')
     news_list = News.objects.filter(content_type=ct).order_by('-created_date').prefetch_related('comments')
+    
+    paginator = Paginator(news_list, RESULTS_PER_PAGE)
+    page = request.GET.get('page')
+    try:
+        news_list = paginator.page(page)
+    except PageNotAnInteger:
+        news_list = paginator.page(1)
+    except EmptyPage:
+        news_list = paginator.page(paginator.num_pages)
+        
     return render(request, 'index.html', {
-        'news_list': news_list[:20],
+        'news_list': news_list,
     })
 
 def news(request, news_id):
@@ -17,8 +31,18 @@ def news(request, news_id):
 
 def games(request):
     games_list = Game.objects.all().order_by('-created_date').prefetch_related('comments')
+
+    paginator = Paginator(games_list, RESULTS_PER_PAGE)
+    page = request.GET.get('page')
+    try:
+        games_list = paginator.page(page)
+    except PageNotAnInteger:
+        games_list = paginator.page(1)
+    except EmptyPage:
+        games_list = paginator.page(paginator.num_pages)
+    
     return render(request, 'games.html', {
-        'games_list': games_list[:20],
+        'games_list': games_list,
     })
 
 def game(request, game_id):
@@ -29,8 +53,18 @@ def game(request, game_id):
 def companies(request):
     ct = ContentType.objects.get(model='company')
     comp_list = Company.objects.filter(content_type=ct).order_by('-created_date').prefetch_related('comments')
+
+    paginator = Paginator(comp_list, RESULTS_PER_PAGE)
+    page = request.GET.get('page')
+    try:
+        comp_list = paginator.page(page)
+    except PageNotAnInteger:
+        comp_list = paginator.page(1)
+    except EmptyPage:
+        comp_list = paginator.page(paginator.num_pages)
+    
     return render(request, 'companies.html', {
-        'comp_list': comp_list[:20],
+        'comp_list': comp_list,
     })
 
 def company(request, comp_id):
